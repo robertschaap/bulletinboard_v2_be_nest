7bc5e0962adb0480faf3db60c73d7cd76c4c13b1
robertschaap <robert.schaap@mac.com>
2020-04-04 14:34:01 +0200

    feat(comment): setup entity and repository

---
+++ src/comments/comment.entity.ts
@@ -0,0 +1,15 @@
+import { Entity, Column, BaseEntity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('posts')
+export class Comment extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  title: string;
+  @Column()
+  body: string;
+  @Column()
+  avatar: string;
+  @Column()
+  name: string;
+}
--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommentRepository } from './comment.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([CommentRepository]),
+  ],
   controllers: [CommentsController],
   providers: [CommentsService]
 })
