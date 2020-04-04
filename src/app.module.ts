bb6411a13a2218ef59f2fec7950c9882d2fb033f
robertschaap <robert.schaap@mac.com>
2020-04-04 13:50:26 +0200

    feat(app): add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CommentsModule } from './comments/comments.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './typeorm.config';
 
 @Module({
-  imports: [CommentsModule],
+  imports: [
+    CommentsModule,
+    TypeOrmModule.forRoot(typeOrmConfig),
+  ],
 })
 export class AppModule {}
