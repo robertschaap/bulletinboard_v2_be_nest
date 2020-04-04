15f265765b205b53d3f6c2650af66cbf750ac411
robertschaap <robert.schaap@mac.com>
2020-04-04 17:22:41 +0200

    feat(comment.repository): add createComment method

--- src/comments/comment.repository.ts
+++ src/comments/comment.repository.ts
@@ -1,9 +1,25 @@
 import { EntityRepository, Repository } from "typeorm";
 import { Comment } from './comment.entity';
+import { CreateCommentDto } from "./create-comment-dto";
 
 @EntityRepository(Comment)
 export class CommentRepository extends Repository<Comment> {
   async getComments(): Promise<Comment[]> {
     return this.find();
+  }
+
+  async createComment(createCommentDto: CreateCommentDto): Promise<Comment> {
+    const { title, body, avatar, name } = createCommentDto;
+
+    const comment = new Comment();
+    comment.title = title;
+    comment.body = body;
+    comment.avatar = avatar;
+    comment.name = name;
+
+    await comment.save();
+
+    return comment;
+
   }
 }
