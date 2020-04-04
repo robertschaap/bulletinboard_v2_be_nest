bc18df0b40ef72815637f0d708a0ad391425e9d0
robertschaap <robert.schaap@mac.com>
2020-04-04 17:25:34 +0200

    feat(comments.service): add creatComment method from repository

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -12,24 +12,7 @@ export class CommentsService {
     return this.commentRepository.getComments();
   }
 
-  private comments: Comment[] = [
-    { id: 1, title: 'title1', body: 'body1', avatar: 'bunny1', name: 'name1' },
-    { id: 2, title: 'title2', body: 'body2', avatar: 'bunny2', name: 'name2' },
-    { id: 3, title: 'title3', body: 'body3', avatar: 'bunny3', name: 'name3' },
-  ];
-
-  createComment(createCommentDto: CreateCommentDto): Comment {
-    const comment = {
-      id: this.generateCommentId(),
-      ...createCommentDto,
-    };
-
-    this.comments.push(comment);
-
-    return comment;
-  }
-
-  private generateCommentId(): number {
-    return this.comments.length + 1;
+  async createComment(createCommentDto: CreateCommentDto): Promise<Comment> {
+    return this.commentRepository.createComment(createCommentDto);
   }
 }
