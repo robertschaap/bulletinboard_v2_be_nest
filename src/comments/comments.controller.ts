6736b11f8234eb11ca469e7f5d028a33f5309dc9
robertschaap <robert.schaap@mac.com>
2020-04-05 10:25:16 +0200

    feat(comments.controller): add apiresponse

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -3,13 +3,8 @@ import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './create-comment-dto';
 import { HttpExceptionFilter } from 'src/htttp-exception.filter';
 import { GetCommentsFilterDto } from './get-comments-filter.dto';
+import { ApiResponse } from 'src/apiresponse';
 
-// TODO: move this into a decorator under the controller and handle errors
-const ApiSuccess = <T>(data: T) => ({
-  status: 'success',
-  data,
-  error: 'null',
-});
 @Controller('comments')
 @UseFilters(new HttpExceptionFilter())
 export class CommentsController {
@@ -17,14 +12,14 @@ export class CommentsController {
 
   @Get()
   async getComments(@Query(ValidationPipe) filterDto: GetCommentsFilterDto ) {
-    return ApiSuccess({
+    return ApiResponse.success({
       comments: await this.commentsService.getComments(filterDto),
     });
   }
 
   @Post()
   async createComment(@Body() createCommentDto: CreateCommentDto) {
-    return ApiSuccess(
+    return ApiResponse.success(
       await this.commentsService.createComment(createCommentDto),
     );
   }
