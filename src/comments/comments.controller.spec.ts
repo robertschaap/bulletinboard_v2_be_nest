7e007b28932b99ece62eb96837b858720654a695
robertschaap <robert.schaap@mac.com>
2020-04-12 14:09:04 +0200

    test(comments.controller): fix test

--- src/comments/comments.controller.spec.ts
+++ src/comments/comments.controller.spec.ts
@@ -2,13 +2,22 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
 
+const mockCommentsService = () => ({
+
+});
+
 describe('CommentsController', () => {
   let controller: CommentsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CommentsController],
-      providers: [CommentsService],
+      providers: [
+        {
+          provide: CommentsService,
+          useFactory: mockCommentsService,
+        },
+      ],
     }).compile();
 
     controller = module.get<CommentsController>(CommentsController);
